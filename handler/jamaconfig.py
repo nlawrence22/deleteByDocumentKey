49e4065ec40562b2f9cdf08c60dcd2212c63b303
ibilal-jama <ibilal@jamasoftware.com>
2017-02-28 12:04:59 -0800

    Separated CONFIG section from non-CONFIG section in jamaconfig.py

--- handler/jamaconfig.py
+++ handler/jamaconfig.py
@@ -1,13 +1,17 @@
 
 class JamaConfig:
     def __init__(self):
+        # please fill in the CONFIG section below:
         self.username = "username"
         self.password = "password"
-        self.auth = (self.username, self.password)
         self.base_url = "https://{base_url}.jamacloud.com"
-        self.rest_url = self.base_url + "/rest/latest/"
         self.itemType = 89008
         self.filename = "deleteDuplicates.csv"
         self.documentKey_column = 2
         self.name_column = 4
+
+
+        # NOT part of the CONFIG section
+        self.auth = (self.username, self.password)
+        self.rest_url = self.base_url + "/rest/latest/"
         self.verify_ssl = True
